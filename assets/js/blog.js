6a02ca651146c0998c8db9b5a855cfcbf59b7eac
unknown <brian.zhen101@gmail.com>
2024-07-04 14:55:29 -0400

    storage now saves properly

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,16 +1,40 @@
 const back = document.querySelector('#back');
 const blogitem = document.querySelector('#blog-item')
 
+let stored = [];
+
 function renderBlog(){
     
     blogitem.innerHTML='';
-    const storedblog = JSON.parse(localStorage.getItem('blog'))
-    console.log(storedblog);
+    
+    console.log(stored);
 
-  //  for (let i = 0; i < storedblog.legnth; i++){
-    //    const stored = storedblog[i];
-      //  console.log(stored);
-   // }
+    for (let i = 0; i < stored.legnth; i++){
+      
+      console.log(stored);
+
+      const ah = document.createElement('a');
+      ah.classList.add('header');
+      ah.textContent = stored[i].title;
+
+      console.log(ah)
+
+      const li = document.createElement('li');
+      li.textContent = stored[i].cont;
+
+      console.log(li);
+
+      const af = document.createElement('a');
+      af.classList.add('footer');
+      af.textContent = stored[i].name;
+
+      console.log(af);
+
+      blogitem.appendChild(ah);
+      blogitem.appendChild(li);
+      blogitem.appendChild(af);
+      
+    }
 
 }
 
@@ -19,4 +43,17 @@ back.addEventListener('click', function(event){
     window.location.href="index.html"
 })
 
-renderBlog();
+function init(){
+  const storedblog = JSON.parse(localStorage.getItem('blog'));
+
+  if(storedblog !== null){
+    stored = storedblog;
+  }
+  
+  console.log(stored);
+  localStorage.setItem('stored',JSON.stringify(stored))
+  renderBlog();
+
+}
+
+init();
--- assets/js/form.js
+++ assets/js/form.js
@@ -3,33 +3,37 @@ const nameInput = document.querySelector('#username');
 const titlecont = document.querySelector('#title');
 const content = document.querySelector('#content');
 const mode = document.querySelector('#sun')
-const conti = document.querySelector('#continue');
+
 
 console.log(nameInput);
 
-const blog = [];
+let blog = [];
+console.log(blog)
 
 submit.addEventListener('click', function (event) {
     event.preventDefault();
+    const prev = JSON.parse(localStorage.getItem('stored'));
+    blog = prev;
+
+    console.log(blog)
+
     const blogelement = {
         name: nameInput.value.trim(),
         title: titlecont.value.trim(),
         cont: content.value.trim()
 
     }
+    
+
     console.log("submit")
     blog.push(blogelement);
     console.log(blog)
     localStorage.setItem('blog', JSON.stringify(blog));
-
+    window.location.href='blog.html';
 
 
 })
 
-conti.addEventListener('click',function(event){
-    event.preventDefault();
-   window.location.href='blog.html';
-})
 
 mode.addEventListener('click', function (event) {
     event.preventDefault();
