938adbdf204ba97cf11bcc0e342aa14c316d43f6
unknown <brian.zhen101@gmail.com>
2024-07-05 00:27:06 -0400

    theme now persists when moving between pages

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,42 @@
 const mode = document.querySelector('#sun')
 const body = document.querySelector('#theme')
 
-let theme = 'light';
+function init() {
+    let theme = localStorage.getItem('theme');
+    console.log(theme)
+    if (theme == null) {
+        theme = 'light';
+    }
+
+    if (theme === 'light') {
+        theme = 'light';
+        body.setAttribute('class', 'light');
+        console.log(theme);
+
+    }
+    else {
+        theme = 'dark';
+        body.setAttribute('class', 'dark');
+        console.log(theme);
+    }
+}
 
 mode.addEventListener('click', function (event) {
-    
+
     event.preventDefault();
 
-    if (theme === 'light'){
+    if (theme === 'light') {
         theme = 'dark';
-        body.setAttribute('class','dark');
+        body.setAttribute('class', 'dark');
+
     }
-    else{
+    else {
         theme = 'light';
-        body.setAttribute('class','light');
+        body.setAttribute('class', 'light');
     }
 
+    localStorage.setItem('theme', theme);
+
 });
+
+init();
